f2e39217a6e34cd728396fc95b271c878a3be69a
Steve Licata <Steve.Licata@gmail.com>
2018-09-14 17:10:22 -0400

    homepage javascript and css

--- public/js/main.js
+++ public/js/main.js
@@ -6,5 +6,30 @@ window.onload = function() {
     toggle: false
   });
 
+  //types "Welcome To . . ." text on home page one letter at a time
+  var myText1 = 'Welcome To . . .';
+  console.log(myText1);
+  var myArray1 = myText1.split("");
+  console.log(myArray1);
+  function looper1() {
+    if(myArray1.length > 0) {
+      document.getElementById("welcome-text").innerHTML += myArray1.shift();
+    }
+    setTimeout(looper1, 100);
+  };
+  looper1();
+
+  //types "JackLambros.com" text on home page one letter at a time.
+  var myText2 = 'JackLambros.com';
+  console.log(myText2);
+  var myArray2 = myText2.split("");
+  console.log(myArray2);
+  function looper2() {
+    if(myArray2.length > 0) {
+      document.getElementById("home-name").innerHTML += myArray2.shift();
+    }
+    setTimeout(looper2, 100);
+  };
+  looper2();
 
 };
